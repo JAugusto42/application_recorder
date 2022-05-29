e19a941dd01234b3616099741681a168471fe028
Jose Augusto <joseaugusto.881@outlook.com>
2022-05-29 10:52:37 -0300

    basic gem structure

---
+++ lib/application_recorder/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ApplicationRecorder
+  VERSION = "0.1.0"
+end
---
+++ spec/application_recorder_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ApplicationRecorder do
+  it "has a version number" do
+    expect(ApplicationRecorder::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
